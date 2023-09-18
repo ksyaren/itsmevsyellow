5af4685367b7de858b687124fa9d78adc8b53174
Yaren Kose <ksyarenn@gmail.com>
2023-09-18 13:03:24 +0300

    final arrangments made

--- Recipes.php
+++ Recipes.php
@@ -4,7 +4,7 @@
    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>itsmevsyellow </title>
+    <title>Recipes </title>
     <!--Connection established between index and css. -->
     <link rel="stylesheet" href="css/style.css">
 
